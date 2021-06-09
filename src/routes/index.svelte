c407ec9f86389b861227697ee1b4289185da9f33
Sebastian Friedel <sebastian@ffwd.consulting>
2021-06-09 10:19:31 +0200

    reproduction case

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
+<script context="module">
+	export const load = async () => ({ props: {} });
+
+</script>
+
+<script>
+	import { session } from '$app/stores';
+
+	if (!import.meta.env.SSR) {
+		$session.test = 'foo';
+	}
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+$session.test: {$session.test}
